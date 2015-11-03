021b7e527874f2e25346f46ddff38998b0d45a66
Mukesh Thotengera <Mukesh.Thotengera@digitalinsight.com>
2015-11-03 10:15:47 -0800

    Initial commit

---
+++ app/src/androidTest/java/com/mthotengera/espressotestingexample/MainActivityInstrumentationTest.java
@@ -0,0 +1,42 @@
+package com.mthotengera.espressotestingexample;
+
+import android.app.Activity;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+/**
+ * Created on 11/3/15.
+ */
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class MainActivityInstrumentationTest{
+    private static final String STRING_TO_BE_TYPED = "Mukesh";
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void sayHello(){
+        onView(withId(R.id.editText)).perform(typeText(STRING_TO_BE_TYPED), closeSoftKeyboard());
+
+        onView(withText("Say hello!")).perform(click());
+
+        String expectedText = "Hello, " + STRING_TO_BE_TYPED + "!";
+        onView(withId(R.id.textView)).check(matches(withText(expectedText)));
+    }
+
+
+}
---
+++ app/src/main/java/com/mthotengera/espressotestingexample/Calculator.java
@@ -0,0 +1,23 @@
+package com.mthotengera.espressotestingexample;
+
+/**
+ * Created on 11/3/15.
+ */
+public class Calculator {
+
+    public double sum(double a, double b){
+        return a + b;
+    }
+
+    public double subtract(double a, double b){
+        return a-b;
+    }
+
+    public double divide(double a, double b){
+        return a / b;
+    }
+
+    public double multiply(double a, double b){
+        return a * b;
+    }
+}
